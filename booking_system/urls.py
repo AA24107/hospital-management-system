da81169c6ff088f5dfa8ff89d0a831e4a6f6fce3
Afeefa Ansari <ansariafeefa7860@gmail.com>
2026-05-19 12:14:39 +0530

    Add authentication pages

--- booking_system/urls.py
+++ booking_system/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('bookings/', views.bookings, name='bookings'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- booking_system/views.py
+++ booking_system/views.py
@@ -1,7 +1,56 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
+
+from .models import Booking, AvailableSlot, User
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'booking_system/index.html')
 
+
+
+
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        confirm_password = request.POST['confirm']
+
+        role = request.POST['role']
+        if role not in ['doctor', 'patient']:
+            return render(request, 'booking_system/register.html', {'error': 'Invalid role'})
+        if password != confirm_password:
+            return render(request, 'booking_system/register.html', {'error': 'Passwords do not match'})
+        
+        user = User.objects.create_user(username=username, password=password, email=email, role=role)
+        login(request, user)
+        return HttpResponseRedirect('/')
+    return render(request, 'booking_system/register.html')
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+        else:
+            messages.error(request, "User doesn't exist")
+            return render(request, "booking_system/login.html")
+        return HttpResponseRedirect(reverse("index"))
+    
+    return render(request, "booking_system/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
